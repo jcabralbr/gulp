07665fc15eb0b88222a9d8123ce0c6b2f4d8b8c8
Julio Cesar Cabral <julio.cabral@b2wdigital.com>
2016-10-20 12:19:29 -0200

    adicionando tarefas clean e copy

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-var gulp = request('gulp');
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean');
 
-gulp.src('src/img')
+gulp.task('copy', ['clean'], function(){
+    return gulp.src('src/**/*')
+        .pipe(gulp.dest('dist'));
 
+});
 
-gulp.dest('src/img')
+gulp.task('clean', function(){
+    // usa return para ser sincrono com a task copy
+    // ja que qualquer dependencia com outra task se nao tiver o return sera assincrono
+    return gulp.src('dist')
+        .pipe(clean());
+});
+
+gulp.task('build-img', ['copy'], function () {
+    gulp.src('dist/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
