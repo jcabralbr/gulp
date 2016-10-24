786f9f956a0afa82887b1ff071c02d0440c36565
Julio Cesar Cabral <julio.cabral@b2wdigital.com>
2016-10-24 15:20:42 -0200

    usando o plugin autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     jshint = require('gulp-jshint'),
     jshintStylish = require('jshint-stylish'),
-    csslint = require('gulp-csslint');
+    csslint = require('gulp-csslint'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', ['copy'], function(){
     gulp.start('build-img', 'usemin');
@@ -37,7 +38,7 @@ gulp.task('usemin', function(){
     gulp.src('dist/**/*.html')
         .pipe(usemin({
             'js': [uglify],
-            'css': [cssmin]
+            'css': [autoprefixer, cssmin]
         }))
         .pipe(gulp.dest('dist'));
 });
